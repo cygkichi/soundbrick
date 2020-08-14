3a12d988708040dcdcb61aa83f23577c790436a9
cygkichi <>
2020-08-15 00:50:28 +0900

    add setup.cfg

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="soundbrick",
-    version="0.0.1",
-    author="cygkichi",
-    author_email="",
-    description="A simple sound file viewer like a brickwork",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cygkichi/soundbrick",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setuptools.setup()
